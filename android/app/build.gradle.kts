49bf2560d9352f2e09fda78b9cdea89703e8a21d
James-joo <jgh3021@naver.com>
2025-08-05 00:02:15 +0900

    fix: ndk 버전 명시적 설정

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.one_shot"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
